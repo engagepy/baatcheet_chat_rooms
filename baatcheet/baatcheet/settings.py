350d21839c07147ad4b2cb99b599ba4c208a41c1
zora89 <zorawar.purohit@gmail.com>
2022-09-07 03:01:05 +0530

    deploy go

--- baatcheet/baatcheet/settings.py
+++ baatcheet/baatcheet/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY=os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://test-astratechztestapp.pagekite.me', 'localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['baatcheetapp.herokuapp.com/','test-astratechztestapp.pagekite.me', 'localhost', '127.0.0.1', '*']
 
 
 # Application definition
