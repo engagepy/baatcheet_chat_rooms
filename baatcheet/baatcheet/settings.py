76da6ec3b4b518bea50ba2e49e918b32544b2ba5
zora89 <zorawar.purohit@gmail.com>
2022-09-07 03:12:11 +0530

    deploy go

--- baatcheet/baatcheet/settings.py
+++ baatcheet/baatcheet/settings.py
@@ -177,6 +177,6 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
-STATIC_ROOT = os.path.join (BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join (BASE_DIR, 'static')
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
