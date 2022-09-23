83899cbb3abeebc79a0340d29ce7ce4ae90b206f
zora89 <zorawar.purohit@gmail.com>
2022-09-24 01:56:07 +0530

    Deploy Updates

--- baatcheet/baatcheet/settings.py
+++ baatcheet/baatcheet/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY=os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['baatcheet-dev.ap-south-1.elasticbeanstalk.com','test-astratechztestapp.pagekite.me', 'localhost', '127.0.0.1']
 
