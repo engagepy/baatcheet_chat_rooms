b75bd6f7f734111aa1124349a41c6ce5d78838bd
zora89 <zorawar.purohit@gmail.com>
2022-09-24 03:41:24 +0530

    Final Pre Deploy

--- baatcheet/baatcheet/wsgi.py
+++ baatcheet/baatcheet/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baatcheet.settings')
+os.environ["DJANGO_SETTINGS_MODULE"] = "baatcheet.settings"
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baatcheet.settings')
 
 application = get_wsgi_application()
