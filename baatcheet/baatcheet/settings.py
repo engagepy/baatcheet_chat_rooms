a6aac54ce9479fff4f0feb84ab926d8b87cf9d64
zora89 <zorawar.purohit@gmail.com>
2022-10-20 23:29:59 +0530

    allowed hosts changed for deployment

--- baatcheet/baatcheet/settings.py
+++ baatcheet/baatcheet/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY=os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['baatcheet-dev.ap-south-1.elasticbeanstalk.com','test-astratechztestapp.pagekite.me', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
