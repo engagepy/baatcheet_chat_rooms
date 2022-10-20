4995bde7aac460bbaf951fbc54a6b87186a1383b
zora89 <zorawar.purohit@gmail.com>
2022-10-21 00:41:38 +0530

    boto3 versions moved around |for  deployment

--- baatcheet/baatcheet/settings.py
+++ baatcheet/baatcheet/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY=os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['65.1.241.70']
 
 
 # Application definition
