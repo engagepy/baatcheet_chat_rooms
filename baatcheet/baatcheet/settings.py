142d320209608acd5e67ce2b5d2d9f9405132d73
zora89 <zorawar.purohit@gmail.com>
2022-11-13 23:13:13 +0530

    debug=False

--- baatcheet/baatcheet/settings.py
+++ baatcheet/baatcheet/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY=os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','35.154.103.55', 'baatcheet.app']
 
