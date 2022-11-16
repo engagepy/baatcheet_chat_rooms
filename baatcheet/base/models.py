037e304f997772d1d6167d078270982c3f187de5
zora89 <zorawar.purohit@gmail.com>
2022-11-16 12:46:41 +0530

    on_delete set to CASCADE in Model

--- baatcheet/base/models.py
+++ baatcheet/base/models.py
@@ -24,8 +24,8 @@ class Topic(models.Model):
 
 
 class Room(models.Model):
-    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    host = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     participants = models.ManyToManyField(User, related_name='participants', blank = True)
