f282b4ee2c47ce9b3344ee65474af255e6664b01
zora89 <zorawar.purohit@gmail.com>
2022-11-16 19:42:57 +0530

    welcome email and thread included on login

--- baatcheet/baatcheet/settings.py
+++ baatcheet/baatcheet/settings.py
@@ -10,11 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
-from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
